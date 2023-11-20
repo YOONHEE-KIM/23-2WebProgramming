a4853c8c2e5b8322a5fe53d1f32c9ade75ba91e2
YOONHEE-KIM <124634807+YOONHEE-KIM@users.noreply.github.com>
2023-11-21 00:41:13 +0900

    데이터가 입력되지 않은 경우
    php 파일에서 결과를 출력할 때, 출력할 데이터가 입력되었는지 확인하는 코드 추가

--- 1120/login.php
+++ 1120/login.php
@@ -8,10 +8,15 @@
 <body>
 <?php
 error_reporting(0);
+
+if (isset($_POST['login']) && isset($_POST['password'])) {
     $login = $_POST['login'];
     $password = $_POST['password'];
 
-    echo $login."님의 비밀번호는 ".$password."입니다.";
+    echo $login . "님의 비밀번호는 " . $password . "입니다.";
+} else {
+    echo "Form 데이터가 전송되지 않았습니다.";
+}
 ?>
 </body>
 </html>
