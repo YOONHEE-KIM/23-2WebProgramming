413457dc2efe7a9a55e33cf6e2227c2a015d2d3e
YOONHEE-KIM <124634807+YOONHEE-KIM@users.noreply.github.com>
2023-12-11 03:40:23 +0900

    Add files via upload

--- letter/letter.js
+++ letter/letter.js
@@ -1,13 +1,51 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.body.addEventListener('click', function (event) {
-        showMemo(event.clientX, event.clientY);
+        var memoContainer = document.getElementById('memoContainer');
+        if (memoContainer.classList.contains('hidden')) {
+            showMemo(event.clientX, event.clientY);
+        }
+        else{hideMemo();}
+    });
+    
+    var memoInput = document.getElementById('memo');
+    memoInput.addEventListener('keydown', function (event) {
+        if (event.key === 'Enter') {
+            // 엔터키가 눌렸을 때 hideMemo 함수 호출
+            hideMemo();
+        }
     });
 });
 
 function showMemo(x, y) {
     var memoContainer = document.getElementById('memoContainer');
+    if (memoContainer.classList.contains('hidden')) {
+        // 메모장이 숨겨져 있으면 열기
+        memoContainer.style.left = x + 'px';
+        memoContainer.style.top = y + 'px';
+        memoContainer.style.opacity = '1';
+        memoContainer.classList.remove('hidden');
+    } else {
+        // 메모장이 열려 있으면 닫기
+        hideMemo();
+    }
+}
+
+function showMemo(x, y) {
+    var memoContainer = document.getElementById('memoContainer');
+    var viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+    var viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    // 메모장의 크기를 고려하여 left와 top을 설정
+    var memoWidth = memoContainer.offsetWidth;
+    var memoHeight = memoContainer.offsetHeight;
+
+    // 현재 클릭된 위치와 메모장의 크기를 고려하여 left와 top을 설정
+    var left = x + memoWidth > viewportWidth ? viewportWidth - memoWidth : x;
+    var top = y + memoHeight > viewportHeight ? viewportHeight - memoHeight : y;
+
     memoContainer.style.left = x + 'px';
     memoContainer.style.top = y + 'px';
+    memoContainer.style.opacity = '1';
     memoContainer.classList.remove('hidden');
 }
 
@@ -15,19 +53,25 @@ function hideMemo() {
 //    memoContainer.classList.add('hidden');
     var memoContainer = document.getElementById('memoContainer');
     var paperAirplane = document.getElementById('paperAirplane');
+    var memoInput = document.getElementById('memo');
 
-    //비행기 돌아감
-    paperAirplane.style.transition = 'transform 1s ease-in-out';
-    paperAirplane.style.transform = 'translate(-50%, -50%) rotate(720deg)';
-    setTimeout(() => {
-        paperAirplane.classList.add('hidden');
-    }, 1000);
+    // 메모장 투명도 변경 (페이드 아웃 효과)
+    memoContainer.style.transition = 'opacity 0.5s ease-out';
+    memoContainer.style.opacity = '0';
 
     // 메모장 숨김
+//    setTimeout(() => {
+//        memoContainer.style.transition = 'opacity 0.5s ease-out'; // 서서히 사라지기
+//        memoContainer.style.opacity = '0';
+//    }, 1000);
+
     setTimeout(() => {
+        paperAirplane.classList.add('hidden');
         memoContainer.classList.add('hidden');
-        memoContainer.style.transition = 'none'; // Remove transition for hiding animation
-    }, 1000);
+        memoInput.value = '';
+        document.body.click();
+    }, 1500);
+
 
     var memoText = document.getElementById('memo').value;
     if (memoText.includes('구구씨') || memoText.includes('구구씨')) {
